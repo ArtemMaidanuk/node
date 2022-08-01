8a6769ebc2bd59dcf1cb6bad46d6690ee889bfb2
ArtemMaidanuk <maidanukartem@gmail.com>
2022-08-01 20:13:32 +0200

    lesson2

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-const fs = require('fs');
-const path = require('path');
 
-const sort = (read, gender, write) => {
-    fs.readdir(path.join(__dirname, read), (err, files) => {
-        if (err) return console.log(err);
 
-        files.forEach((file) => {
-                const readFolder = path.join(__dirname, read, file)
-                fs.readFile(readFolder, (err, data) => {
-                    if (err) return console.log(err);
 
-                    const user =JSON.parse(data.toString());
-
-                    if(user.gender=== gender) {
-                        fs.rename(readFolder, path.join(__dirname,write,file),(err)=>{
-                            if(err) return console.log(err);
-                        });
-                    }
-                });
-            });
-    });
-}
-sort('boys', "female", 'girls');
-sort('girls', "male", 'boys');
 
 
 
