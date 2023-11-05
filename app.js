383142ade503cfe15837f5300f5df07df6ffb39b
ArtemMaidanuk <maidanukartem@gmail.com>
2023-11-05 21:02:05 +0300

    mongo

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/node');
+
 const usersRouter = require("./routers/user.router");
-const axios = require('axios')
-
-
-
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.get('/',async (reg,res)=>{
-    console.log(reg)
-    const resp = await axios.get('https://jsonplaceholder.typicode.com/users')
-    res.status(resp.status).json(resp.data)
-})
-
 
 app.use('/users', usersRouter)
+
+app.use ('*',(req,res)=>{
+    res.status(404).json('Rout not found')
+})
+
+app.use((err,req,res,next)=>{
+   res
+       .status(err.status || 500)
+       .json({
+           error:err.message || 'Unknow error',
+           code : err.status || 500
+    })
+})
 
 app.listen(5000, ()=>{
     console.log('Server listen on port 5000')
